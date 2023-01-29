59aeaecfb788673eca6d1da4f4c3e8d1d1b345c1
Nate Wang <ytowang@ucdavis.edu>
2023-01-29 00:53:14 -0800

    Use MERN template

---
+++ client/src/api/index.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const url = 'http://localhost:5000/posts';
+
+const fetchPosts = () => axios.get(url);
+
+export const api = axios.create({
+  baseURL: 'https://api.github.com/'
+})
