351a05869a0a8878bb0791ce4ab0107cff46ca88
Nate Wang <ytowang@ucdavis.edu>
2023-02-07 16:19:56 -0800

    Update port for https

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use('/posts', postRouter);
 
 const CONNECTION_URL = 'mongodb+srv://onedayw:onedayet@nufcblog-db.hlvt83k.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 443;
 
 mongoose.set('strictQuery', false);
 
